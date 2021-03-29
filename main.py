5d512b7439ba83eaa13f4f6a74633dbdda890d87
Atj Rupesh <62754636+atjrupesh@users.noreply.github.com>
2021-03-29 12:21:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-
 print('Welcome to band name genartor\n')
 
 city = input('Which city you are grew up in\n')
 
 pet = input('What is the name of the pet you had? \n')
 
-print("Your band name could be " city + " " +pet )
-
+print("Your band name could be "+  city + " " + pet)
